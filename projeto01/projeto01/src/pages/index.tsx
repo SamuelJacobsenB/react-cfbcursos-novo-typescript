76be1ba2a0f6e96d7e1def081d59e0edd73da096
Samuel Jacobsen Barcellos <rcbsjbarcellos@gmail.com>
2024-07-24 13:22:43 -0300

    Aula 04

--- projeto01/projeto01/src/pages/index.tsx
+++ projeto01/projeto01/src/pages/index.tsx
@@ -1,8 +1,16 @@
 export default function Home() {
+
+  const nome: string = 'Samuel';
+  const curso: string = 'React Next';
+
+  function retNome(): string {
+    return nome;
+  };
+
   return (
     <main>
-      <div>Curso de React Next</div>
-      <div>Samuel</div>
+      <div>{curso}</div>
+      <div>{retNome()}</div>
       <div>Typescript</div>
       <div>React</div>
     </main>
