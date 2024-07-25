446100475ed271da6a0ee3c4de2926e014db902a
Samuel Jacobsen Barcellos <rcbsjbarcellos@gmail.com>
2024-07-25 11:47:54 -0300

    Aula 12

--- projeto01/projeto01/src/pages/index.tsx
+++ projeto01/projeto01/src/pages/index.tsx
@@ -11,15 +11,58 @@ export default function Home() {
     return val - (desc / 2);
   };
 
+  const produtos: object[] = [
+    {
+      produto: 'Mouse',
+      valor: 59.90,
+      desconto: 5,
+      disponivel: true
+    },
+    {
+      produto: 'Teclado',
+      valor: 159.90,
+      desconto: 20,
+      disponivel: true
+    },
+    {
+      produto: 'Monitor',
+      valor: 1240.00,
+      desconto: 115,
+      disponivel: true
+    },
+    {
+      produto: 'CPU',
+      valor: 829.90,
+      desconto: 59.90,
+      disponivel: true
+    },
+    {
+      produto: 'Caixa de Som',
+      valor: 280.90,
+      desconto: 24.90,
+      disponivel: true
+    },
+    {
+      produto: 'Celular',
+      valor: 1890.90,
+      desconto: 105.90,
+      disponivel: true
+    }
+  ];  
+
   return (
     <main>
       <Topo/>
-      <div>Typescript</div>
-      <div>React</div>
-      <div className="flex justify-center">
-        <Card produto={'Mouse'} valor={59.90} desconto={0} funcao={calcDesc}/>
-        <Card produto={'Teclado'} valor={159.90} desconto={10.00} funcao={calcDesc2} />
-        <Card produto={'Monitor'} valor={1250.00} desconto={50.00} funcao={calcDesc2}/>
+      <div className="special">React - Typescript</div>
+      <div className="list">
+        {
+          produtos.map((el: any, i: number)=>{
+            return (
+                <Card produto={el.produto} valor={el.valor} desconto={el.desconto} funcao={calcDesc} key={i}/>
+            );
+          })
+    
+        }
       </div>
     </main>
   );
