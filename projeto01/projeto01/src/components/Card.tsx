cb1f47e34a16400ef552c1c43f4013264eeccb3c
Samuel Jacobsen Barcellos <rcbsjbarcellos@gmail.com>
2024-07-25 07:34:46 -0300

    Aula 08

--- projeto01/projeto01/src/components/Card.tsx
+++ projeto01/projeto01/src/components/Card.tsx
@@ -1,6 +1,9 @@
-const Card = () => {
+const Card = (props: any) => {
   return (
-    <div>Card</div>
+    <div className="mr-10 border">
+        <div>{props.produto}</div>
+        <div>{props.valor}</div>
+    </div>
   );
 };
 
--- projeto01/projeto01/src/pages/index.tsx
+++ projeto01/projeto01/src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Topo from "@/components/Topo";
+import Card from "@/components/Card";
 
 export default function Home() {
   return (
@@ -6,6 +7,11 @@ export default function Home() {
       <Topo></Topo>
       <div>Typescript</div>
       <div>React</div>
+      <div className="flex justify-center">
+        <Card produto={'mouse'} valor={'59,90'}/>
+        <Card produto={'teclado'} valor={'159,90'}/>
+        <Card produto={'monitor'} valor={'1250.00'}/>
+      </div>
     </main>
   );
 };
