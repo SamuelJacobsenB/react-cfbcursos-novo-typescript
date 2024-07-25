928fecdfd13ea07f12dacbf994381e76bd147e38
Samuel Jacobsen Barcellos <rcbsjbarcellos@gmail.com>
2024-07-25 11:22:15 -0300

    Aula 11

--- projeto01/projeto01/src/components/Card.tsx
+++ projeto01/projeto01/src/components/Card.tsx
@@ -7,10 +7,25 @@ interface CardProps {
 
 const Card = (props: CardProps) => {
   return (
-    <div className="mr-10 border p-4">
+    <div className={`mr-10 border ${props.desconto > 0 ? 'border-blue-600' : 'border-red-600'}  p-4`}>
         <div>Produto: {props.produto}</div>
-        <div>Valor à vista com desconto de R${props.desconto.toFixed(2)} sem a taxa  : {props.funcao(props.valor, props.desconto).toFixed(2)} com a taxa</div>
-        <div>Valor à prazo: {props.valor.toFixed(2)}</div>
+
+        {props.desconto > 0 ?
+          (
+            <>
+              <div>Valor à vista com desconto de R${props.desconto.toFixed(2)} sem a taxa  : R${props.funcao(props.valor, props.desconto).toFixed(2)} com a taxa</div>
+
+              <div>Valor à prazo: {props.valor.toFixed(2)}</div>
+            </>
+          )
+            :
+          (
+            <>
+              <div>Este produto não tem desconto.</div>
+              <div>Valor à vista e à prazo: R${props.valor.toFixed(2)}</div>
+            </>
+          )
+        }
     </div>
   );
 };
--- projeto01/projeto01/src/pages/index.tsx
+++ projeto01/projeto01/src/pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home() {
       <div>Typescript</div>
       <div>React</div>
       <div className="flex justify-center">
-        <Card produto={'Mouse'} valor={59.90} desconto={5.00} funcao={calcDesc}/>
+        <Card produto={'Mouse'} valor={59.90} desconto={0} funcao={calcDesc}/>
         <Card produto={'Teclado'} valor={159.90} desconto={10.00} funcao={calcDesc2} />
         <Card produto={'Monitor'} valor={1250.00} desconto={50.00} funcao={calcDesc2}/>
       </div>
